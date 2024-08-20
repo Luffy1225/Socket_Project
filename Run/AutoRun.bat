3b3270ba148d2fd72197aa32decd6410cc605a28
Luffy <luffy20041225@gmail.com>
2024-08-20 15:18:20 +0800

    新增 Todo / Run資料夾 (更新 變成跟Main Branch 一樣的架構)

---
+++ Run/AutoRun.bat
@@ -0,0 +1,35 @@
+@echo off
+setlocal
+
+REM 讀取RunParameter.ini中的參數，並儲存在局部變數中
+for /f "tokens=2 delims==" %%a in ('findstr "ClientCount" RunParameter.ini') do (
+    set "ClientCount=%%a"
+)
+
+for /f "tokens=2 delims==" %%a in ('findstr "ServerName" RunParameter.ini') do (
+    set "ServerName=%%a"
+)
+
+for /f "tokens=2 delims==" %%a in ('findstr "ConnectIP" RunParameter.ini') do (
+    set "ConnectIP=%%a"
+)
+
+for /f "tokens=2 delims==" %%a in ('findstr "ConnectPort" RunParameter.ini') do (
+    set "ConnectPort=%%a"
+)
+
+REM 去除多餘的引號
+set "ServerName=%ServerName:"=%"
+set "ConnectIP=%ConnectIP:"=%"
+set "ConnectPort=%ConnectPort:"=%"
+
+REM 執行Server_Socket.exe，不等待結束
+ start "" Server_Socket.exe %ServerName%
+
+
+REM 根據ClientCount的數量執行Client_Socket.exe，不等待結束
+for /l %%i in (1,1,%ClientCount%) do (
+    start "" Client_Socket.exe %ConnectIP% %ConnectPort%
+)
+
+endlocal
---
+++ Todo.md
@@ -0,0 +1,31 @@
+# Branch: ReadKey TODO 清單
+
+## 主要目標
+
+- [ ] 改進成 可以直接使用的 遊戲 Library(提出時間 2024-08-20)
+- [ ] Main Branch 功能 移植(提出時間 2024-08-20)
+- [ ] 新增功能 Server 對 Client 下達 Ban()指令 (提出時間 2024-08-20)
+- [ ] 改成 OnMessage() 觸發事件 (提出時間 2024-08-19)
+- [ ] 改善 Server/Client 關閉問題 (提出時間 2024-08-12)
+
+ 
+
+## 次要目標
+
+- [ ] 更新 ReadKey Branch :README 文件
+
+
+## 已完成項目 (上往下 近到遠)
+
+
+
+
+## Todo 撰寫範例
+
+- [ ] 任務 (提出時間 YYYY-MM-DD)
+- ~~[x] 已完成 (提出時間 YYYY-MM-DD /完成日期: YYYY-MM-DD)~~
+
+```markdown
+- [ ] 任務 (提出時間 YYYY-MM-DD)
+- ~~[x] 已完成 (提出時間 YYYY-MM-DD /完成日期: YYYY-MM-DD)~~
+```
